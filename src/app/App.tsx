a42bdc09bfd0b582eb053cfae75c188a06877667
Nickinfo1 <nickinfo1@yandex.ru>
2023-07-16 12:10:30 +0500

    edit code for more clear state

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,10 +4,10 @@ import { classNames } from 'shared/lib';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 
 const App = () => {
-    const { theme, toggleTheme } = useTheme();
+    const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
